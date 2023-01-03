56d1262ef71b09906580a96f8f784a9fd466d0b1
fyodorio <alinetofyodor@gmail.com>
2023-01-04 02:15:41 +0500

    chore: add image integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import vue from "@astrojs/vue";
+import image from "@astrojs/image";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue(), image()]
 });
